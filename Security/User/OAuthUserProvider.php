1584a77cd73e60ef24078ff3d508251f23c50b3d
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-15 15:14:56 +0300

    Replace deprecated ClassUtils::getClass() with get_class().

--- Security/User/OAuthUserProvider.php
+++ Security/User/OAuthUserProvider.php
@@ -15,7 +15,6 @@ use Darvin\UserBundle\Repository\UserRepository;
 use Darvin\UserBundle\Security\OAuth\Exception\BadResponseException;
 use Darvin\UserBundle\Security\OAuth\Response\DarvinAuthResponse;
 use Darvin\UserBundle\User\UserFactory;
-use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManager;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
@@ -113,8 +112,8 @@ class OAuthUserProvider implements OAuthAwareUserProviderInterface, UserProvider
      */
     public function refreshUser(UserInterface $user)
     {
-        if (!$this->supportsClass(ClassUtils::getClass($user))) {
-            throw new UnsupportedUserException(sprintf('User class "%s" is not supported.', ClassUtils::getClass($user)));
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException(sprintf('User class "%s" is not supported.', get_class($user)));
         }
 
         return $this->loadUserByUsername($user->getUsername());
