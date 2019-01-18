3513f712db66b47ba55cf68a08ffca743b1532c9
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-18 16:00:31 +0300

    Refactor admin view widgets.

--- Admin/View/UserRolesWidget.php
+++ Admin/View/UserRolesWidget.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2016, Darvin Studio
+ * @copyright Copyright (c) 2016-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -50,10 +50,10 @@ class UserRolesWidget extends AbstractWidget
     /**
      * {@inheritdoc}
      */
-    protected function createContent($entity, array $options, $property)
+    protected function createContent($entity, array $options): ?string
     {
         $parts = [];
-        $roles = $this->getPropertyValue($entity, $property);
+        $roles = $this->getPropertyValue($entity, $options['property']);
 
         if (!is_array($roles) && !$roles instanceof \Traversable) {
             $roles = [$roles];
@@ -76,7 +76,7 @@ class UserRolesWidget extends AbstractWidget
     /**
      * {@inheritdoc}
      */
-    protected function getRequiredPermissions()
+    protected function getRequiredPermissions(): array
     {
         return [
             Permission::VIEW,
