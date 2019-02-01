28d2eeae43faa04104d713e199f78ca4f81b5214
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-01 14:33:08 +0300

    Update copyrights.

--- Event/SecurityEvents.php
+++ Event/SecurityEvents.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2016, Darvin Studio
+ * @copyright Copyright (c) 2016-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
