ab5a7d7cae5a0882ee47684a714308d53f24013e
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-07 09:35:10 +0300

    Update copyrights.

--- Entity/RegistrationConfirmToken.php
+++ Entity/RegistrationConfirmToken.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author    Lev Semin <lev@darvin-studio.ru>
- * @copyright Copyright (c) 2017, Darvin Studio
+ * @copyright Copyright (c) 2017-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
