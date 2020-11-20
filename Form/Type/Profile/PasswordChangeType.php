b1f8e1f8068c8f93c6961ef81c00354575c6f41c
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-20 08:35:01 +0300

    Update copyrights.

--- Form/Type/Profile/PasswordChangeType.php
+++ Form/Type/Profile/PasswordChangeType.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2019, Darvin Studio
+ * @copyright Copyright (c) 2019-2020, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
